0d6ee5e168e0f5990406dbca97ca23d41d14dcf7
MG2356 <98271284+MG2356@users.noreply.github.com>
2022-06-29 20:27:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HomeScreen from "./screens/HomeScreen";
+import Nav from "./Components/Nav";
+function App() {
+  return (
+    <div className="App">
+<Nav />
+      <HomeScreen />
+    </div>
+  );
+}
+
+export default App;
